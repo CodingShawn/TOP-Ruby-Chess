d9fad260e8e033910ba0a6cc097f967052ab7e4f
Shawn <codingshawn@gmail.com>
2020-07-10 21:42:29 +0800

    Refactored board class to change @squares variable to 8 arrays of 8

--- lib/board.rb
+++ lib/board.rb
@@ -7,16 +7,19 @@ class Board
   
   def initialize
     @squares = []
-    64.times {@squares.append(Square.new)}
+    8.times do
+      row_array = []
+      8.times {row_array.append(Square.new)}
+      @squares.append(row_array)
+    end
   end
 
   def draw_board
     puts ROW_BORDERS
-    (8).downto(1) do |row|
+    8.times do |row|
       row_image = "#{row} "
-      (8).downto(1) do |file|
-        square_no = row * file - 1
-        row_image += "| #{@squares[square_no].to_s} "
+      8.times do |file|
+        row_image += "| #{@squares[row][file].to_s} "
       end
       row_image += "|"
       puts row_image
