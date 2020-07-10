3485501de8c0f7e6653bf01c52e648a80e0fa5de
Shawn <codingshawn@gmail.com>
2020-07-10 22:13:33 +0800

    Updated Piece class to include instance variables for row, column, colour, and possible moves. Also created function to prevent off board moves

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,8 +1,13 @@
 class Piece
   attr_accessor :type
-  def initialize(location)
+  attr_reader :possible_moves
+  def initialize(location, colour)
     @type = nil
     @location = location
+    @column = @location[0]
+    @row = @location[1]
+    @colour = colour
+    @possible_moves = possible_moves
   end
 
   def move(end_location)
@@ -10,5 +15,19 @@ class Piece
   end
 
   def is_move_legal
+  end
+
+  def possible_moves
+  end
+
+  def prevent_off_board_moves(initial_possible_moves)
+    on_board_moves = initial_possible_moves.filter do |location|
+      if location[0] < 0 || location[0] > 7 || location[1] < 0 || location[1] > 7
+        false
+      else
+        true
+      end
+    end
+    on_board_moves
   end
 end 
