702fdfcd4c6f38bebbb224f6b4bc2befd5113ff3
Shawn <codingshawn@gmail.com>
2020-07-11 20:08:11 +0800

    Minor changes to Piece class and created methods to return possible paths after taking into account blocking pieces
    -Removed @possible_moves, pieces will only generate possible moves when possible_moves called from Board class
    -possible_moves now take occupied_squares as an argument so able to determine which board space is taken up
    -Created check_if_route_blocked to determine where path is blocked
    -Created remove_blocked_path to remove from possible moves where path is blocked from piece

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,13 +1,12 @@
 class Piece
   attr_accessor :type
-  attr_reader :possible_moves
-  def initialize(location, colour)
+  attr_reader :location
+  def initialize(location = [99,99], colour = nil)
     @type = nil
     @location = location
     @column = @location[0]
     @row = @location[1]
     @colour = colour
-    @possible_moves = possible_moves
   end
 
   def move(end_location)
@@ -17,7 +16,7 @@ class Piece
   def is_move_legal
   end
 
-  def possible_moves
+  def possible_moves(occupied_squares)
   end
 
   def prevent_off_board_moves(initial_possible_moves)
@@ -29,5 +28,22 @@ class Piece
       end
     end
     on_board_moves
+  end
+  
+  def check_if_route_blocked(route, occupied_squares)
+    unless route.empty?
+      index = route.index {|path| occupied_squares.include? path}
+    else
+      -1
+    end
+  end
+
+  def remove_blocked_path(route, occupied_squares)
+    index = check_if_route_blocked(route, occupied_squares)
+    route = route[0..index]
+  end
+  
+  def to_s
+    " "
   end
 end 
