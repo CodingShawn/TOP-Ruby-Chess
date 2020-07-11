cd64ebdf4449bbcf7a6de38cc25995db766eb3d1
Shawn <codingshawn@gmail.com>
2020-07-11 20:39:10 +0800

    Created change_turn method in Game class to allow for changing of turns

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,17 @@
 require_relative 'board'
 
 class Game
+  attr_reader :turn
   def initialize
     @board = Board.new
+    @turn = "white"
   end
 
+  def change_turn
+    if @turn.eql? "white"
+      @turn = "black"
+    else
+      @turn = "white"
+    end
+  end
 end
