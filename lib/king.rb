17337585b11b53aba924e55bbaddf90e5d55b9f5
Shawn <codingshawn@gmail.com>
2020-07-12 14:53:17 +0800

    Moved responsibility of checking check status back to Board class instead of King class

--- lib/king.rb
+++ lib/king.rb
@@ -7,18 +7,16 @@ class King < Piece
     @type = "king"
   end
 
-  def possible_moves(occupied_squares, opponent_zone_of_control)
+  def possible_moves(occupied_squares)
     result = [[@column - 1, @row], [@column - 1, @row + 1], [@column, @row + 1],
               [@column + 1, @row + 1], [@column + 1, @row], [@column + 1, @row - 1],
               [@column, @row - 1], [@column - 1, @row - 1]]
-    result -= opponent_zone_of_control
-    #prevent king from moving in to square where it will get checked
     prevent_off_board_moves(result)
   end
 
   def to_s
     unicode = ""
-    if @colour == "white"
+    if @colour == "black"
       unicode = "\u2654"
     else
       unicode = "\u265A"
