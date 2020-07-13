180cbd46dac3430745f9328e6e513be43b14e56a
Shawn <codingshawn@gmail.com>
2020-07-13 20:51:24 +0800

    For Knight class, added possible_moves method

--- lib/knight.rb
+++ lib/knight.rb
@@ -5,6 +5,13 @@ class Knight < Pawn
   end
 
   def possible_moves(occupied_squares)
+    #occupied_squares redundant as knight does not get blocked
+    result = [
+      [@column + 2, @row + 1], [@column + 2, @row - 1], 
+      [@column - 2, @row + 1], [@column - 2, @row - 1],
+      [@column + 1, @row + 2], [@column - 1, @row + 2],
+      [@column + 1, @row - 2], [@column - 1, @row - 2]]
+    prevent_off_board_moves(result)
   end
 
   def to_s
